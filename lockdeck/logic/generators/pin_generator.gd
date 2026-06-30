6f6169b5eebf59e2a0d64be8f964154339bef2f8
starshinePerigee <skywing.ki@gmail.com>
2026-06-30 10:34:05 -0700

    Fix pin generator logic

--- lockdeck/logic/generators/pin_generator.gd
+++ lockdeck/logic/generators/pin_generator.gd
@@ -1,5 +1,10 @@
 class_name PinGenerator
 
+## Gets a random position for valid playable pins (index 1 - 7)
+## Index 0 is base, and index 8 is final.
+static func random_pos() -> int:
+	return randi_range(1, PinSpec.PIN_DEPTH_COUNT - 2)
+
 static func get_known_test_pin() -> PinSpec:
 	var spec := PinSpec.new()
 	for i in range(1, PinSpec.PIN_DEPTH_COUNT - 1):
@@ -10,24 +15,37 @@ static func get_known_test_pin() -> PinSpec:
 
 static var FILLER_DEPTHS: Array[Depths] = [Depths.FORCE, Depths.JAM]
 
+## Gets a random "filler" depth
+static func get_filler() -> Depths:
+	return FILLER_DEPTHS[
+		randi_range(
+		0, 
+		len(FILLER_DEPTHS) - 1
+		)
+	]
+
 static func get_random_base_pin(difficulty_mod: int = 0) -> PinSpec:
 	var spec := PinSpec.new()
 	for i in range(1, PinSpec.PIN_DEPTH_COUNT - 1):
 		spec.depths[i] = Depths.EMPTY
-		
-	var key_loc := randi_range(1, 8)
-	spec.depths[key_loc] = Depths.KEY
 	
-	if key_loc < 8:
-		spec.depths[randi_range(key_loc + 1, 8)] = Depths.BREAK
-		
+	var pos_1 := random_pos()
+	var pos_2 := pos_1
+	while pos_2 == pos_1:
+		pos_2 = random_pos()
+	
+	spec.depths[min(pos_1, pos_2)] = Depths.WARN
+	spec.depths[max(pos_1, pos_2)] = Depths.BREAK
+	
 	for i in range(randi_range(0, difficulty_mod)):
-		if spec.depths[i] == Depths.EMPTY:
-			spec.depths[i] = Depths.BREAK
+		var p := random_pos()
+		if spec.depths[p] == Depths.EMPTY:
+			spec.depths[p] = Depths.BREAK
 	
 	for i in range(randi_range(0, 5)):
-		if spec.depths[i] == Depths.EMPTY:
-			spec.depths[i] = FILLER_DEPTHS[randi_range(0, len(FILLER_DEPTHS) - 1)]
+		var p := random_pos()
+		if spec.depths[p] == Depths.EMPTY:
+			spec.depths[p] = get_filler()
 	
 	spec.pin_set = false
 	return spec
