7b118774d1c975393ed8f39f051be5d89e3d658d
starshinePerigee <skywing.ki@gmail.com>
2026-07-03 11:48:26 -0700

    New background and some update logic

--- lockdeck/objects/cylinder/cylinders.gd
+++ lockdeck/objects/cylinder/cylinders.gd
@@ -76,9 +76,7 @@ func current_active_pin() -> int:
 
 func force_update() -> void:
 	if len(_active_pins) > 0:
-		# grab the middle one
-		@warning_ignore("integer_division")
-		new_pin_hovered.emit(_active_pins[len(_active_pins) / 2])
+		new_pin_hovered.emit(_active_pins[-1])
 
 # Becasue mouse regions don't overlap (the way card drag areas do) 
 # the logic is a little different:
