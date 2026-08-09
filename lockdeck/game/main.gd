44c1dfabaf907dfeec3436d592f5bb2a16b83b78
starshinePerigee <skywing.ki@gmail.com>
2026-08-08 19:52:47 -0700

    Begin outlining theme

--- lockdeck/game/main.gd
+++ lockdeck/game/main.gd
@@ -1,7 +1,7 @@
 extends Control
 ## This is the top level entrypoint for Handful of Lockpicks
 
-var VERSION_NUMBER := "v0.11.0"
+var VERSION_NUMBER := "v0.11.1"
 
 func start_game(starter_deck: Array[CardSpec]) -> void:
 	$MenuButton.visible = true
