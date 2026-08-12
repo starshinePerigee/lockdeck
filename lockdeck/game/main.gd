e54172ef3192c33206ef7127a66bcefb78eae484
starshinePerigee <skywing.ki@gmail.com>
2026-08-12 02:42:21 -0700

    update version

--- lockdeck/game/main.gd
+++ lockdeck/game/main.gd
@@ -1,7 +1,7 @@
 extends Control
 ## This is the top level entrypoint for Handful of Lockpicks
 
-var VERSION_NUMBER := "v0.12.0"
+var VERSION_NUMBER := "v0.12.1-big-two"
 
 func start_game(starter_deck: Array[CardSpec]) -> void:
 	$MenuButton.visible = true
