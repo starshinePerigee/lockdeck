34d694c9aed026fabc5326ded8115892a00e84f8
starshinePerigee <skywing.ki@gmail.com>
2026-08-21 09:36:59 -0700

    s/settings/menu widget

--- lockdeck/game/menus/settings_main.gd
+++ lockdeck/game/menus/settings_main.gd
@@ -3,13 +3,13 @@ extends Control
 ## Show the settings. Hiding them will be handled by settings widget itself.
 func show_settings():
 	visible = true
-	$SettingsWidget.show_widget()
+	$MenuWidget.show_widget()
 
 func do_nothing():
 	pass
 
 func _ready() -> void:
-	$SettingsWidget.closed.connect(hide)
-	$SettingsWidget.add_button("Button 1", do_nothing)
-	$SettingsWidget.add_button("Button 2", do_nothing)
-	$SettingsWidget.add_button("Button 3", do_nothing, true)
+	$MenuWidget.closed.connect(hide)
+	$MenuWidget.add_button("Button 1", do_nothing)
+	$MenuWidget.add_button("Button 2", do_nothing)
+	$MenuWidget.add_button("Button 3", do_nothing, true)
--- lockdeck/game/meta/menu_main.gd
+++ lockdeck/game/meta/menu_main.gd
@@ -9,12 +9,12 @@ signal break_three
 ## Show the main menu. Hiding it will be handled by the widget itself.
 func show_menu():
 	visible = true
-	$SettingsWidget.show_widget()
+	$MenuWidget.show_widget()
 
 func _ready() -> void:
-	$SettingsWidget.closed.connect(hide)
-	$SettingsWidget.add_button("Save and return to title", return_to_title.emit, true)
-	$SettingsWidget.add_button("Game settings", open_settings.emit, true)
-	$SettingsWidget.add_button("DEBUG: Solve level", auto_complete_level.emit, true)
-	$SettingsWidget.add_button("DEBUG: Reveal lock", reveal_level.emit, true)
-	$SettingsWidget.add_button("DEBUG: Break three", break_three.emit) 
+	$MenuWidget.closed.connect(hide)
+	$MenuWidget.add_button("Save and return to title", return_to_title.emit, true)
+	$MenuWidget.add_button("Game settings", open_settings.emit, true)
+	$MenuWidget.add_button("DEBUG: Solve level", auto_complete_level.emit, true)
+	$MenuWidget.add_button("DEBUG: Reveal lock", reveal_level.emit, true)
+	$MenuWidget.add_button("DEBUG: Break three", break_three.emit) 
