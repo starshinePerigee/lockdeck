81354d5aa82b5cb697013d7a9c698ea2fc4457dc
starshinePerigee <skywing.ki@gmail.com>
2026-08-31 23:05:30 -0700

    Upate itch files

--- lockdeck/game/meta/menu_main.gd
+++ lockdeck/game/meta/menu_main.gd
@@ -6,6 +6,19 @@ signal auto_complete_level
 signal reveal_level
 signal break_three
 
+
+var screenshot_number := 0
+
+func request_screenshot() -> void:
+	get_tree().create_timer(0.1).timeout.connect(take_screenshot)
+	
+
+func take_screenshot() -> void:
+	await RenderingServer.frame_post_draw
+	var img = get_viewport().get_texture().get_image()
+	img.save_png("user://screenshot_%s.png" % screenshot_number)
+	screenshot_number += 1
+
 ## Show the main menu. Hiding it will be handled by the widget itself.
 func show_menu():
 	visible = true
@@ -18,3 +31,4 @@ func _ready() -> void:
 	$MenuWidget.add_button("DEBUG: Solve level", auto_complete_level.emit, true)
 	$MenuWidget.add_button("DEBUG: Reveal lock", reveal_level.emit, true)
 	$MenuWidget.add_button("DEBUG: Break three", break_three.emit) 
+	$MenuWidget.add_button("DEBUG: Screenshot", request_screenshot, true) 
